74987fa2ba3b5c7b6d766fc1a9e13eb3a1384c4f
ektarathi <rathiekta2801@gmail.com>
2021-01-21 22:40:12 +0000

    Updated config file

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,5 @@
 import * as firebase from 'firebase';
 
-/*var config = {
-    apiKey: "AIzaSyAgLah13y7Dn_-JGyqtUJQqImSnTx_XkYs",
-    authDomain: "contactform-2a89b.firebaseapp.com",
-    databaseURL: "https://contactform-2a89b.firebaseio.com",
-    projectId: "contactform-2a89b",
-    storageBucket: "contactform-2a89b.appspot.com",
-    messagingSenderId: "761366534738",
-    appId: "1:761366534738:web:eeb1eb2d39cb05cc095d8b",
-    measurementId: "G-0HGPQKTP80"
-  };
-*/
-
 var config = {
     apiKey: "AIzaSyDovqxwjBcIqUr8b_681AXgTwHtmKdKqe8",
     authDomain: "user-authentication-9d891.firebaseapp.com",
